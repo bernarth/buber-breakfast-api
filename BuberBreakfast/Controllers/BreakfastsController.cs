dd4f99438df57412549696da3eed4902eea727ae
Bernarth <alberthbernarth75@gmail.com>
2023-03-21 21:41:31 -0400

    enahance controller

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -4,24 +4,25 @@ using BuberBreakfast.Contracts.BuberBreakfast;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
+[Route("[controller]")]
 public class BreakfastsController : ControllerBase
 {
-    [HttpPost("/breakfasts")]
+    [HttpPost()]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request) {
         return Ok(request);
     }
 
-    [HttpGet("/breakfasts/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id) {
         return Ok(id);
     }
 
-    [HttpPut("/breakfasts/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request) {
         return Ok(request);
     }
 
-    [HttpDelete("/breakfasts/{id:guid}")]
+    [HttpDelete("{id:guid}")]
     public IActionResult DeleteBreakfast(Guid id) {
         return Ok(id);
     }
